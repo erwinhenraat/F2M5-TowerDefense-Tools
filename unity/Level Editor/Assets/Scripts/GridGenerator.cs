346d8bcb0df5a07897d3b5cebce721c2979a82b7
Erwin Henraat <e.henraat@ma-web.nl>
2021-07-15 16:23:56 +0200

    little cleanup

--- unity/Level Editor/Assets/Scripts/GridGenerator.cs
+++ unity/Level Editor/Assets/Scripts/GridGenerator.cs
@@ -14,36 +14,25 @@ public class GridGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //ToDO
         //Serializable Tile class maken - V
         //Serializable JsonDataObject class maken - V
         //inladen van JSON file - V
         //inladen van lijst met tiles vanuit JSON - V
-
+        //Loop door lijst met tiles heen - V
+        //plaats plane prefabs afhankelijk van type - V
         jdo = JsonDataObject.CreateJSONData(jsonFile.text);
-
-      
-       
         
-      
-        
-        //Loop door lijst met tiles heen 
-
-        //plaats plane prefabs afhankelijk van type
-
         if (tilePrefabs.Length == 0){
             throw new Exception("tilePrefabs is empty");
         }else{
             GenerateGrid();
         }
-        
-
-
     }
     private void GenerateGrid(){
         int index = 0;
         foreach (Tile t in jdo.tiles)
-        {
-            
+        {            
             GameObject prefab = tilePrefabs[0];
             switch(t.state){
                 case "default":
@@ -62,21 +51,15 @@ public class GridGenerator : MonoBehaviour
                 prefab = tilePrefabs[4];
                 break;
             }
+
             float gridWidth = jdo.width;
             float tileSize =  prefab.GetComponent<Renderer>().bounds.size.x;
             float xpos = index % gridWidth * tileSize;
             float zpos = Mathf.Floor(index / gridWidth)*tileSize; 
             
-
             Instantiate(prefab, new Vector3(xpos,0f,zpos), Quaternion.identity , transform);
             
-            if(index < jdo.tiles.Length)index++;
+            index++;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
